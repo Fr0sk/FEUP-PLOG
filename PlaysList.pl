c3ea2d5598a1e7f17d8f4215f7bc8b2163a1a794
Filipe Coelho <the.fr0sk@gmail.com>
2016-11-13 15:46:15 +0000

    Added single piece move list predicates

---
+++ PlaysList.pl
@@ -0,0 +1,20 @@
+/*
+ * Player 1 can only go sides and down
+ * Board, Player, PieceCoords (X, Y);
+ * Returns List with the possible moves
+ */
+/*plSinglePiece(Board, 1, X, Y, List) :-*/
+
+/*
+ * Player 2 can only go sides and up
+ * Board, Player, PieceCoords (X, Y);
+ * Returns List with the possible moves
+ */
+/*plSinglePiece(Board, 1, X, Y, List) :-*/
+
+/*
+ * Regroup moves can go any direction
+ * Board, Player, PieceCoords (X, Y);
+ * Returns List with the possible moves
+ */
+ /*plRegroupSinglePiece(Board, Player, X, Y, List)*/

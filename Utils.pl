d9ca7c9f092d41b54607303f7b938afbf077e36f
Filipe Coelho <the.fr0sk@gmail.com>
2016-11-13 15:46:03 +0000

    Added predicates to translate columns into indexes

--- Utils.pl
+++ Utils.pl
@@ -7,4 +7,24 @@ uSwitchPlayer(Player, NextPlayer):-
         Player==1 -> NextPlayer is 2;
         Player==2 -> NextPlayer is 1
     ).
-    
+
+uTranslateColumn('A', 0).
+uTranslateColumn('B', 1).
+uTranslateColumn('C', 2).
+uTranslateColumn('D', 3).
+uTranslateColumn('E', 4).
+uTranslateColumn('F', 5).
+uTranslateColumn('G', 6).
+uTranslateColumn('H', 7).
+uTranslateColumn('I', 8).
+uTranslateColumn('J', 9).
+uTranslateColumn('a', 0).
+uTranslateColumn('b', 1).
+uTranslateColumn('c', 2).
+uTranslateColumn('d', 3).
+uTranslateColumn('e', 4).
+uTranslateColumn('f', 5).
+uTranslateColumn('g', 6).
+uTranslateColumn('h', 7).
+uTranslateColumn('i', 8).
+uTranslateColumn('j', 9).
